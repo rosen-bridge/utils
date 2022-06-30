74e1cc5340e9c44d41b45a0c5540c1ef1313824a
sahandzou <sahandzoufan79@gmail.com>
2022-06-30 10:52:13 +0430

    update TokenMap class

--- lib/TokenMap/TokenMap.ts
+++ lib/TokenMap/TokenMap.ts
@@ -1,18 +1,29 @@
 import { TokensMap } from "./types";
-import * as _ from "lodash";
+import tokensList from "../tokensList/tokensList.json"
 
+/**
+ * TokenMap class searches for different assets properties in different chains
+ */
 export class TokenMap{
     private tokensConfig: TokensMap
 
-    constructor(tokens: TokensMap) {
+    constructor(tokens: TokensMap = tokensList) {
         this.tokensConfig = tokens;
     }
 
+    /**
+     * it searches tokens for data specified in `filter` and returns data specified
+     *  in `result` function input
+     * @param filter
+     *  example: {chain: "ergo", condition: {tokenID:"tokenID"}}
+     * @param result
+     *  example: {chain: "cardano", value:["fingerprint"]}
+     */
     search = (
         filter: { chain: string, condition: { [key: string]: string } },
         result: { chain: string, value: Array<string> }
     ) => {
-        const res = this.tokensConfig.tokens.filter((token) => {
+        return this.tokensConfig.tokens.filter((token) => {
             if (Object.hasOwnProperty.call(token, filter.chain)) {
                 const chain = token[filter.chain];
                 for (const [key, val] of Object.entries(filter.condition)) {
@@ -28,11 +39,9 @@ export class TokenMap{
                 return false;
             }
             return true;
-        })
-
-        if (res.length > 0) {
-            if (Object.hasOwnProperty.call(res[0], result.chain)) {
-                const chain = res[0][result.chain];
+        }).map((token) => {
+            if (Object.hasOwnProperty.call(token, result.chain)) {
+                const chain = token[result.chain];
                 for (const key of Object.keys(chain)) {
                     if (!result.value.includes(key)) {
                         delete chain[key]
@@ -40,8 +49,8 @@ export class TokenMap{
                 }
                 return chain;
             }
-        }
-        return {}
+            return {}
+        });
     }
 
 }
--- tests/TokenMap.ts
+++ tests/TokenMap.ts
@@ -1,120 +1,62 @@
-// import firstSample from './samples/firstSample.json'
-// import { Chain, TokensMap } from "../lib/TokenMap/types";
-// import { TokenMap } from "../lib/TokenMap/TokenMap";
-// import { expect } from "chai";
-//
-// describe("TokenMap", () => {
-//     describe("searchChain", () => {
-//         it("checks for wrong search key", () => {
-//             const map: TokensMap = firstSample;
-//             const tokenMap = new TokenMap(map);
-//             for (let i = 0; i < 3; i++) {
-//                 const res = tokenMap.searchChain(map.tokens[i], Chain.ERGO, "wrongKey");
-//                 expect(res.result).to.be.false;
-//             }
-//         })
-//
-//         it("checks for tokenId in ergo", () => {
-//             const map: TokensMap = firstSample;
-//             const tokenMap = new TokenMap(map);
-//             for (let i = 0; i < 3; i++) {
-//                 const res = tokenMap.searchChain(map.tokens[i], Chain.ERGO, "tokenID");
-//                 expect(res.result).to.be.true;
-//                 expect(res.searchResult).to.be.eql(map.tokens[i].ergo.tokenID);
-//             }
-//         })
-//
-//         it("checks for tokenName in ergo", () => {
-//             const map: TokensMap = firstSample;
-//             const tokenMap = new TokenMap(map);
-//             for (let i = 0; i < 3; i++) {
-//                 const res = tokenMap.searchChain(map.tokens[i], Chain.ERGO, "tokenName");
-//                 expect(res.result).to.be.true;
-//                 expect(res.searchResult).to.be.eql(map.tokens[i].ergo.tokenName);
-//             }
-//         })
-//
-//         it("checks for fingerprint in cardano", () => {
-//             const map: TokensMap = firstSample;
-//             const tokenMap = new TokenMap(map);
-//             for (let i = 0; i < 3; i++) {
-//                 const res = tokenMap.searchChain(map.tokens[i], Chain.CARDANO, "fingerprint");
-//                 expect(res.result).to.be.true;
-//                 expect(res.searchResult).to.be.eql(map.tokens[i].cardano.fingerprint);
-//             }
-//         })
-//
-//         it("checks for policyID in cardano", () => {
-//             const map: TokensMap = firstSample;
-//             const tokenMap = new TokenMap(map);
-//             for (let i = 0; i < 3; i++) {
-//                 const res = tokenMap.searchChain(map.tokens[i], Chain.CARDANO, "policyID");
-//                 expect(res.result).to.be.true;
-//                 expect(res.searchResult).to.be.eql(map.tokens[i].cardano.policyID);
-//             }
-//         })
-//
-//         it("checks for assetID in cardano", () => {
-//             const map: TokensMap = firstSample;
-//             const tokenMap = new TokenMap(map);
-//             for (let i = 0; i < 3; i++) {
-//                 const res = tokenMap.searchChain(map.tokens[i], Chain.CARDANO, "assetID");
-//                 expect(res.result).to.be.true;
-//                 expect(res.searchResult).to.be.eql(map.tokens[i].cardano.assetID);
-//             }
-//         })
-//     })
-//
-//     describe("search", () => {
-//         it("checks assetID in ergo should mapped to correct fingerprint in cardano", () => {
-//             const map: TokensMap = firstSample;
-//             const tokenMap = new TokenMap(map);
-//             const res = tokenMap.search(
-//                 {
-//                     chain: Chain.ERGO,
-//                     search: {
-//                         key: "tokenID",
-//                         value: "1111111111111111111111111111111111111111111111111111111111111111"
-//                     }
-//                 },
-//                 {chain: Chain.CARDANO, value: "fingerprint"}
-//             );
-//             expect(res.result).to.be.true;
-//             expect(res.searchResult).to.be.eql(map.tokens[0].cardano.fingerprint)
-//         })
-//
-//         it("checks assetID in ergo should mapped to correct token name in ergo", () => {
-//             const map: TokensMap = firstSample;
-//             const tokenMap = new TokenMap(map);
-//             const res = tokenMap.search(
-//                 {
-//                     chain: Chain.ERGO,
-//                     search: {
-//                         key: "tokenID",
-//                         value: "1111111111111111111111111111111111111111111111111111111111111111"
-//                     }
-//                 },
-//                 {chain: Chain.ERGO, value: "tokenName"}
-//             );
-//             expect(res.result).to.be.true;
-//             expect(res.searchResult).to.be.eql(map.tokens[0].ergo.tokenName)
-//         })
-//
-//         it("checks policyID in cardano should mapped to first correct tokenID in ergo", () => {
-//             const map: TokensMap = firstSample;
-//             const tokenMap = new TokenMap(map);
-//             const res = tokenMap.search(
-//                 {
-//                     chain: Chain.CARDANO,
-//                     search: {
-//                         key: "policyID",
-//                         value: "policyID3"
-//                     }
-//                 },
-//                 {chain: Chain.ERGO, value: "tokenID"}
-//             );
-//             expect(res.result).to.be.true;
-//             expect(res.searchResult).to.be.eql(map.tokens[2].ergo.tokenID)
-//         })
-//     })
-// })
+import firstSample from './samples/firstSample.json'
+import { TokensMap } from "../lib/TokenMap/types";
+import { TokenMap } from "../lib";
+import { expect } from "chai";
+
+describe("TokenMap", () => {
+    describe("search", () => {
+        it("checks should return assets finger print in cardano", () => {
+            const map: TokensMap = firstSample;
+            const tokenMap = new TokenMap(map);
+            const res = tokenMap.search(
+                {
+                    chain: "cardano",
+                    condition: {
+                        policyID: "22222222222222222222222222222222222222222222222222222222",
+                        assetID: "3333333333333333333333333333333333333333333333333333333333333333333333333333"
+                    }
+                }, {
+                    chain: "cardano",
+                    value: ["fingerprint"]
+                })
+            expect(res.length).to.be.eql(1);
+            expect(res[0]).to.be.eql({fingerprint: "asset111111111111111111111111111111111111111"})
+        });
+
+        it("returns asset fingerprint in cardano with ergo tokenID", () => {
+            const map: TokensMap = firstSample;
+            const tokenMap = new TokenMap(map);
+            const res = tokenMap.search(
+                {
+                    chain: "ergo",
+                    condition: {
+                        tokenID: "tokenId",
+                    }
+                }, {
+                    chain: "cardano",
+                    value: ["fingerprint"]
+                })
+            expect(res.length).to.be.eql(1);
+            expect(res[0]).to.be.eql({fingerprint: "asset3fingerprint"})
+        });
+
+        it("returns ergo tokenID with cardano asset fingerPrint", () => {
+            const map: TokensMap = firstSample;
+            const tokenMap = new TokenMap(map);
+            const res = tokenMap.search(
+                {
+                    chain: "cardano",
+                    condition: {
+                        fingerprint: "asset122222222222222222222222222222222222222",
+                    }
+                }, {
+                    chain: "ergo",
+                    value: ["tokenID"]
+                })
+            expect(res.length).to.be.eql(1);
+            expect(res[0]).to.be.eql({tokenID: "2222222222222222222222222222222222222222222222222222222222222222"})
+        });
+
+    })
+
+})
