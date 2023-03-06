31877aefa607f13282f98d7155f70fbba58fd666
Mohammad Kermani <mkermani144@gmail.com>
2023-03-06 17:17:13 +0330

    Add `findLastReleasesWith` to utils
    Use the function to find the last releases matching some conditions.

--- packages/utils/lib/utils/github.ts
+++ packages/utils/lib/utils/github.ts
@@ -1,10 +1,13 @@
 import { Octokit } from 'octokit';
+import { compact } from 'lodash-es';
 
 import {
   CONTRACT_REPO_NAME,
   DEFAULT_RELEASES_FETCHING_PAGE_SIZE,
   ROSEN_BRIDGE_ORGANIZATION,
 } from '../constants';
+
+import { GithubRelease } from '../types';
 
 /**
  * Fetch a page of releases from Github Api in each iteration until there are no
@@ -35,4 +38,32 @@ async function* fetchReleasesPage(
   }
 }
 
-export { fetchReleasesPage };
+/**
+ * For any predicate in a list, find the last release matching that predicate
+ * and return corresponding releases list. If all releases are iterated and no
+ * matching release is found, return `null` for that predicate.
+ * @param predicates a list of predicates we want to find a matching release for
+ */
+const findLastReleasesWith = async (
+  predicates: ((release: GithubRelease) => boolean)[]
+) => {
+  const releasesPageIterator = fetchReleasesPage();
+
+  const result: GithubRelease[] = Array(predicates.length).fill(null);
+
+  for await (const releasesPage of releasesPageIterator) {
+    predicates.forEach((predicate, index) => {
+      const foundRelease = releasesPage.find(predicate);
+      if (foundRelease) {
+        result[index] = foundRelease;
+      }
+    });
+
+    if (compact(result).length === predicates.length) {
+      return result;
+    }
+  }
+  return result;
+};
+
+export { fetchReleasesPage, findLastReleasesWith };
--- packages/utils/tests/utils/github.spec.ts
+++ packages/utils/tests/utils/github.spec.ts
@@ -1,6 +1,9 @@
 import { Octokit } from 'octokit';
 
-import { fetchReleasesPage } from '../../lib/utils/github';
+import {
+  fetchReleasesPage,
+  findLastReleasesWith,
+} from '../../lib/utils/github';
 
 import { releases } from './github.data';
 import { DEFAULT_RELEASES_FETCHING_PAGE_SIZE } from '../../lib/constants';
@@ -48,5 +51,87 @@ describe('fetchReleasesPage', () => {
     expect((await iterator.next()).value?.length).toBe(4);
     expect((await iterator.next()).value).toBe(undefined);
     expect((await iterator.next()).done).toBe(true);
+  });
+});
+
+describe('findLastReleasesWith', () => {
+  /**
+   * Target:
+   * It should find last releases correctly
+   *
+   * Dependencies:
+   * - mocked Octokit
+   *
+   * Scenario:
+   * N/A
+   *
+   * Expected output:
+   * The function should return correct releases
+   */
+  it('should find last releases correctly', async () => {
+    (Octokit as jest.MockedClass<typeof Octokit>).mockImplementation(() => {
+      let page = 0;
+      return {
+        rest: {
+          repos: {
+            listReleases: (async () => {
+              const currentIndex = DEFAULT_RELEASES_FETCHING_PAGE_SIZE * page;
+              page += 1;
+              return {
+                data: releases.slice(currentIndex, currentIndex + 5),
+              };
+            }) as any,
+          },
+        },
+      } as any;
+    });
+
+    const foundReleases = await findLastReleasesWith([
+      (release) => release.id === 2,
+      (release) => release.id === 5,
+    ]);
+
+    expect(foundReleases[0].id).toBe(2);
+    expect(foundReleases[1].id).toBe(5);
+  });
+
+  /**
+   * Target:
+   * It should return null for any predicate with no matching release
+   *
+   * Dependencies:
+   * - mocked Octokit
+   *
+   * Scenario:
+   * N/A
+   *
+   * Expected output:
+   * The function should return null if no matching release is found
+   */
+  it('should return null for any predicate with no matching release', async () => {
+    (Octokit as jest.MockedClass<typeof Octokit>).mockImplementation(() => {
+      let page = 0;
+      return {
+        rest: {
+          repos: {
+            listReleases: (async () => {
+              const currentIndex = DEFAULT_RELEASES_FETCHING_PAGE_SIZE * page;
+              page += 1;
+              return {
+                data: releases.slice(currentIndex, currentIndex + 5),
+              };
+            }) as any,
+          },
+        },
+      } as any;
+    });
+
+    const foundReleases = await findLastReleasesWith([
+      (release) => release.id === 100,
+      (release) => release.id === 2,
+    ]);
+
+    expect(foundReleases[0]).toBe(null);
+    expect(foundReleases[1].id).toBe(2);
   });
 });
