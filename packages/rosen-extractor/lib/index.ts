083d66ac77a3404a4fdbc9ba60908c9d5840bacd
HFazelinia <hadifazelinia78@gmail.com>
2023-03-12 04:52:54 +0000

    export AbstractRosenDataExtractor

--- packages/rosen-extractor/lib/index.ts
+++ packages/rosen-extractor/lib/index.ts
@@ -1,3 +1,4 @@
+export { default as AbstractRosenDataExtractor } from './getRosenData/abstract/AbstractRosenDataExtractor';
 export { CardanoKoiosRosenExtractor } from './getRosenData/cardano/CardanoKoiosRosenExtractor';
 export { CardanoOgmiosRosenExtractor } from './getRosenData/cardano/CardanoOgmiosRosenExtractor';
 export { ErgoNodeRosenExtractor } from './getRosenData/ergo/ErgoNodeRosenExtractor';
