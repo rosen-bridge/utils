846b847b2e2c8643d8ad22780fde1b9018535c51
Mohammad Kermani <mkermani144@gmail.com>
2023-03-13 09:30:59 +0330

    Wrap `downloadRosenAssets` tests in a `describe` to match RIP

--- packages/utils/tests/downloadRosenAssets.spec.ts
+++ packages/utils/tests/downloadRosenAssets.spec.ts
@@ -11,155 +11,157 @@ import { mockOctokit } from './mocks/octokit.mock';
 
 jest.mock('download');
 
-/**
- * Target:
- * It should download Rosen assets correctly
- *
- * Dependencies:
- * - mocked Octokit
- *
- * Scenario:
- * N/A
- *
- * Expected output:
- * N/A
- */
-it('should download Rosen assets correctly', async () => {
-  mockOctokit();
+describe('downloadRosenAssets', () => {
+  /**
+   * Target:
+   * It should download Rosen assets correctly
+   *
+   * Dependencies:
+   * - mocked Octokit
+   *
+   * Scenario:
+   * N/A
+   *
+   * Expected output:
+   * N/A
+   */
+  it('should download Rosen assets correctly', async () => {
+    mockOctokit();
 
-  await downloadRosenAssets('mainnet', 'rosen');
+    await downloadRosenAssets('mainnet', 'rosen');
 
-  expect(download).toHaveBeenCalledWith(
-    mainNetStableRelease.assets[0].browser_download_url,
-    'rosen',
-    {
-      filename: 'contracts-awesomechain.json',
-    }
-  );
-  expect(download).toHaveBeenCalledWith(
-    mainNetStableRelease.assets[1].browser_download_url,
-    'rosen',
-    {
-      filename: 'tokensMap.json',
-    }
-  );
-});
+    expect(download).toHaveBeenCalledWith(
+      mainNetStableRelease.assets[0].browser_download_url,
+      'rosen',
+      {
+        filename: 'contracts-awesomechain.json',
+      }
+    );
+    expect(download).toHaveBeenCalledWith(
+      mainNetStableRelease.assets[1].browser_download_url,
+      'rosen',
+      {
+        filename: 'tokensMap.json',
+      }
+    );
+  });
 
-/**
- * Target:
- * It should download Rosen assets correctly when including prereleases
- *
- * Dependencies:
- * - mocked Octokit
- *
- * Scenario:
- * N/A
- *
- * Expected output:
- * N/A
- */
-it('should download Rosen assets correctly when including prereleases', async () => {
-  mockOctokit();
+  /**
+   * Target:
+   * It should download Rosen assets correctly when including prereleases
+   *
+   * Dependencies:
+   * - mocked Octokit
+   *
+   * Scenario:
+   * N/A
+   *
+   * Expected output:
+   * N/A
+   */
+  it('should download Rosen assets correctly when including prereleases', async () => {
+    mockOctokit();
 
-  await downloadRosenAssets('mainnet', 'rosen', true);
+    await downloadRosenAssets('mainnet', 'rosen', true);
 
-  expect(download).toHaveBeenCalledWith(
-    mainNetPrereleaseRelease.assets[0].browser_download_url,
-    'rosen',
-    {
-      filename: 'contracts-awesomechain.json',
-    }
-  );
-  expect(download).toHaveBeenCalledWith(
-    mainNetPrereleaseRelease.assets[1].browser_download_url,
-    'rosen',
-    {
-      filename: 'tokensMap.json',
-    }
-  );
-});
+    expect(download).toHaveBeenCalledWith(
+      mainNetPrereleaseRelease.assets[0].browser_download_url,
+      'rosen',
+      {
+        filename: 'contracts-awesomechain.json',
+      }
+    );
+    expect(download).toHaveBeenCalledWith(
+      mainNetPrereleaseRelease.assets[1].browser_download_url,
+      'rosen',
+      {
+        filename: 'tokensMap.json',
+      }
+    );
+  });
 
-/**
- * Target:
- * It should download Rosen assets and add a suffix correctly
- *
- * Dependencies:
- * - mocked Octokit
- *
- * Scenario:
- * N/A
- *
- * Expected output:
- * N/A
- */
-it('should download Rosen assets and add a suffix correctly', async () => {
-  mockOctokit();
+  /**
+   * Target:
+   * It should download Rosen assets and add a suffix correctly
+   *
+   * Dependencies:
+   * - mocked Octokit
+   *
+   * Scenario:
+   * N/A
+   *
+   * Expected output:
+   * N/A
+   */
+  it('should download Rosen assets and add a suffix correctly', async () => {
+    mockOctokit();
 
-  await downloadRosenAssets('mainnet', 'rosen', false, 'suffix');
+    await downloadRosenAssets('mainnet', 'rosen', false, 'suffix');
 
-  expect(download).toHaveBeenCalledWith(
-    mainNetStableRelease.assets[0].browser_download_url,
-    'rosen',
-    {
-      filename: 'contracts-awesomechain-suffix.json',
-    }
-  );
-  expect(download).toHaveBeenCalledWith(
-    mainNetStableRelease.assets[1].browser_download_url,
-    'rosen',
-    {
-      filename: 'tokensMap-suffix.json',
-    }
-  );
-});
+    expect(download).toHaveBeenCalledWith(
+      mainNetStableRelease.assets[0].browser_download_url,
+      'rosen',
+      {
+        filename: 'contracts-awesomechain-suffix.json',
+      }
+    );
+    expect(download).toHaveBeenCalledWith(
+      mainNetStableRelease.assets[1].browser_download_url,
+      'rosen',
+      {
+        filename: 'tokensMap-suffix.json',
+      }
+    );
+  });
 
-/**
- * Target:
- * It should download Rosen assets correctly
- *
- * Dependencies:
- * - mocked Octokit
- * - empty mocked download package
- *
- * Scenario:
- * N/A
- *
- * Expected output:
- * N/A
- */
-it('should not call `download` function if no matching release is found', async () => {
-  mockOctokit();
-  jest.mocked(download).mockClear();
+  /**
+   * Target:
+   * It should download Rosen assets correctly
+   *
+   * Dependencies:
+   * - mocked Octokit
+   * - empty mocked download package
+   *
+   * Scenario:
+   * N/A
+   *
+   * Expected output:
+   * N/A
+   */
+  it('should not call `download` function if no matching release is found', async () => {
+    mockOctokit();
+    jest.mocked(download).mockClear();
 
-  await downloadRosenAssets('no-release-net', 'rosen');
+    await downloadRosenAssets('no-release-net', 'rosen');
 
-  expect(download).toHaveBeenCalledTimes(0);
-});
+    expect(download).toHaveBeenCalledTimes(0);
+  });
 
-/**
- * Target:
- * It should throw an error if an error happens
- *
- * Dependencies:
- * - mocked Octokit
- * - mocked download package (throwing an error)
- *
- * Scenario:
- * N/A
- *
- * Expected output:
- * N/A
- */
-it('should throw an error if an error happens', async () => {
-  mockOctokit();
-  jest.mocked(download).mockRejectedValue(new Error('Bad!'));
+  /**
+   * Target:
+   * It should throw an error if an error happens
+   *
+   * Dependencies:
+   * - mocked Octokit
+   * - mocked download package (throwing an error)
+   *
+   * Scenario:
+   * N/A
+   *
+   * Expected output:
+   * N/A
+   */
+  it('should throw an error if an error happens', async () => {
+    mockOctokit();
+    jest.mocked(download).mockRejectedValue(new Error('Bad!'));
 
-  const downloadPromise = downloadRosenAssets(
-    'mainnet',
-    'rosen',
-    false,
-    'suffix'
-  );
+    const downloadPromise = downloadRosenAssets(
+      'mainnet',
+      'rosen',
+      false,
+      'suffix'
+    );
 
-  await expect(downloadPromise).rejects.toThrow('');
+    await expect(downloadPromise).rejects.toThrow('');
+  });
 });
