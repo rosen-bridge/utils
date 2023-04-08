bfe43c1ee55bd03b508e877e2606e38d8d8d0aa6
sahandzou <sahandzoufan97@gmail.com>
2023-04-08 13:02:29 +0330

    reforamt logs

--- packages/tss/lib/constants/constants.ts
+++ packages/tss/lib/constants/constants.ts
@@ -6,7 +6,7 @@ const registerType: MessageType = 'register';
 
 const registerTimeout = 2 * 60 * 1000; // 2 minutes
 const heartbeatTimeout = 1 * 60 * 1000; // 1 minutes
-const timestampTolerance = 1 * 60 * 1000; // 1 minute
+const messageValidDuration = 1 * 60 * 1000; // 1 minute
 const guardsUpdateStatusInterval = 3 * 1000; // 3 seconds
 
 export {
@@ -15,6 +15,6 @@ export {
   registerType,
   registerTimeout,
   heartbeatTimeout,
-  timestampTolerance,
+  messageValidDuration,
   guardsUpdateStatusInterval,
 };
