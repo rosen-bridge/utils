83ff6424811305d3dd53f6d734fa90fd0fa64c29
HFazelinia <hadifazelinia78@gmail.com>
2023-04-27 22:39:07 +0100

    fix rosen-extractor tokenMap test data

--- packages/rosen-extractor/tests/getRosenData/TestUtils.ts
+++ packages/rosen-extractor/tests/getRosenData/TestUtils.ts
@@ -16,23 +16,39 @@ export default class TestUtils {
       {
         [ERGO_CHAIN]: {
           tokenId: ERGO_NATIVE_TOKEN,
+          metaData: {
+            type: 'tokenType',
+            residency: 'tokenResidency',
+          },
         },
         [CARDANO_CHAIN]: {
           fingerprint:
             'ace7bcc2ce705679149746620de3a84660ce57573df54b5a096e39a2',
           policyId: 'ace7bcc2ce705679149746620de3a84660ce57573df54b5a096e39a2',
           assetName: '7369676d61',
+          metaData: {
+            type: 'tokenType',
+            residency: 'tokenResidency',
+          },
         },
       },
       {
         [ERGO_CHAIN]: {
           tokenId:
             'f6a69529b12a7e2326acffee8383e0c44408f87a872886fadf410fe8498006d3',
+          metaData: {
+            type: 'tokenType',
+            residency: 'tokenResidency',
+          },
         },
         [CARDANO_CHAIN]: {
           fingerprint: CARDANO_NATIVE_TOKEN,
           tokenId: CARDANO_NATIVE_TOKEN,
           id: CARDANO_NATIVE_TOKEN,
+          metaData: {
+            type: 'tokenType',
+            residency: 'tokenResidency',
+          },
         },
       },
     ],
@@ -48,12 +64,20 @@ export default class TestUtils {
         [ERGO_CHAIN]: {
           tokenId:
             'f6a69529b12a7e2326acffee8383e0c44408f87a872886fadf410fe8498006d3',
+          metaData: {
+            type: 'tokenType',
+            residency: 'tokenResidency',
+          },
         },
         [CARDANO_CHAIN]: {
           fingerprint:
             'ace7bcc2ce705679149746620de3a84660ce57573df54b5a096e39a2',
           policyId: 'ace7bcc2ce705679149746620de3a84660ce57573df54b5a096e39a2',
           assetName: '7369676d61',
+          metaData: {
+            type: 'tokenType',
+            residency: 'tokenResidency',
+          },
         },
       },
     ],
