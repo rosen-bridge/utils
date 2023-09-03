68424abd642f61eabb8de6c2fe30a885cabfa655
Warrior-7 <warrior7.dev@gmail.com>
2023-09-03 17:31:00 +0330

    add fetch box implementations for explorer and node

--- packages/boxes/rwt-repo/lib/rwt-repo.ts
+++ packages/boxes/rwt-repo/lib/rwt-repo.ts
@@ -1,4 +1,6 @@
 import { AbstractLogger, DummyLogger } from '@rosen-bridge/logger-interface';
+import ergoExplorerClientFactory from '@rosen-clients/ergo-explorer';
+import ergoNodeClientFactory from '@rosen-clients/ergo-node';
 
 export class RWTRepoBuilder {
   constructor(
@@ -27,6 +29,50 @@ export class RWTRepo {
     private networkUrl: string,
     private logger: AbstractLogger = new DummyLogger()
   ) {}
+
+  // TODO: implement this
+  updateBox(trackMempool: boolean) {
+    return trackMempool;
+  }
+
+  async getBoxFromExplorer() {
+    const explorer = ergoExplorerClientFactory(this.networkUrl);
+    const itemsInfo = await explorer.v1.getApiV1BoxesUnspentByaddressP1(
+      this.repoAddress
+    );
+
+    const repoBoxes = itemsInfo.items?.filter((item) =>
+      item.assets?.some((asset) => asset.tokenId === this.repoNft)
+    );
+
+    if (repoBoxes === undefined || repoBoxes.length <= 0) {
+      const error = new Error(
+        `no unspent boxes wtih address "${this.repoAddress}" and NFT-ID "${this.repoNft}" were found by explorer`
+      );
+      this.logger.error(error.message);
+      throw error;
+    }
+
+    return repoBoxes[0];
+  }
+
+  async getBoxFromNode() {
+    const node = ergoNodeClientFactory(this.networkUrl);
+    const boxes = await node.getBoxesByAddressUnspent(this.repoAddress);
+    const repoBoxes = boxes.filter((box) =>
+      box.assets?.some((asset) => asset.tokenId === this.repoNft)
+    );
+
+    if (repoBoxes.length <= 0) {
+      const error = new Error(
+        `no unspent boxes wtih address "${this.repoAddress}" and NFT-ID "${this.repoNft}" were found by Ergo node`
+      );
+      this.logger.error(error.message);
+      throw error;
+    }
+
+    return repoBoxes[0];
+  }
 }
 
 export enum ErgoNetworkType {
