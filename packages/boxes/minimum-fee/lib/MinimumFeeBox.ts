043b0f65f90d2813d5e0f6e3b5872c855f57ef76
HFazelinia <hadifazelinia78@gmail.com>
2023-09-26 09:32:06 +0100

    replace logger-interface with abstract-logger

--- packages/boxes/minimum-fee/lib/MinimumFeeBox.ts
+++ packages/boxes/minimum-fee/lib/MinimumFeeBox.ts
@@ -5,7 +5,7 @@ import ergoExplorerClientFactory from '@rosen-clients/ergo-explorer';
 import ergoNodeClientFactory from '@rosen-clients/ergo-node';
 import JsonBigInt from '@rosen-bridge/json-bigint';
 import handleApiError from './handleApiError';
-import { AbstractLogger, DummyLogger } from '@rosen-bridge/logger-interface';
+import { AbstractLogger, DummyLogger } from '@rosen-bridge/abstract-logger';
 import { MinimumFeeBoxBuilder } from './MinimumFeeBoxBuilder';
 import { MinimumFeeConfig } from './MinimumFeeConfig';
 
