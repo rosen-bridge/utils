8f8f1bb2015347cab6fa212fef2ab9088a5209cb
Warrior-7 <warrior7.dev@gmail.com>
2023-09-27 23:29:24 +0330

    correct debug log and unit test scenario to resolve merge request comments

--- packages/boxes/rwt-repo/lib/rwtRepo.ts
+++ packages/boxes/rwt-repo/lib/rwtRepo.ts
@@ -390,9 +390,13 @@ export class RWTRepo {
     let widIndex = r4Hex.slice(1).indexOf(wid);
     widIndex = widIndex === -1 ? widIndex : widIndex + 1;
 
-    this.logger.debug(
-      `index of wid=[${wid}] found in R4: index=${widIndex}, R4[${widIndex}]=${r4Hex[widIndex]}`
-    );
+    if (widIndex !== -1) {
+      this.logger.debug(
+        `index of wid=[${wid}] found in R4: index=[${widIndex}], R4[${widIndex}]=[${r4Hex[widIndex]}]`
+      );
+    } else {
+      this.logger.debug(`index of wid=[${wid}] not found in R4`);
+    }
 
     return widIndex;
   }
