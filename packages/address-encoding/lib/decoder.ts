2779b9a12dfc92b67b3e6b240eb626d37501c43b
HFazelinia <hadifazelinia78@gmail.com>
2024-02-05 10:59:51 +0000

    add address decoder

--- packages/address-encoding/lib/decoder.ts
+++ packages/address-encoding/lib/decoder.ts
@@ -1,9 +1,34 @@
+import { BITCOIN_CHAIN, CARDANO_CHAIN, ERGO_CHAIN } from './const';
+import { UnsupportedAddress, UnsupportedChain } from './types';
+import * as ergoLib from 'ergo-lib-wasm-nodejs';
+import * as cardanoLib from '@emurgo/cardano-serialization-lib-nodejs';
+import * as bitcoinLib from 'bitcoinjs-lib';
+
 /**
  * decodes address of a chain
- *  hexAddress length cannot be more than 57 bytes
+ *  encodedAddress length cannot be more than 57 bytes
  * @param chain
- * @param hexAddress
+ * @param encodedAddress
  */
-export const addressDecoder = (chain: string, hexAddress: string) => {
-  throw Error(`not implemented`);
+export const addressDecoder = (chain: string, encodedAddress: string) => {
+  if (encodedAddress.length > 57 * 2)
+    throw new UnsupportedAddress(chain, encodedAddress);
+  let address: string;
+  switch (chain) {
+    case ERGO_CHAIN:
+      address = ergoLib.Address.from_public_key(
+        Buffer.from(encodedAddress, 'hex')
+      ).to_base58(ergoLib.NetworkPrefix.Mainnet);
+      break;
+    case CARDANO_CHAIN:
+      address = cardanoLib.Address.from_bytes(
+        Buffer.from(encodedAddress, 'hex')
+      ).to_bech32();
+      break;
+    case BITCOIN_CHAIN: // TODO: implement this
+      throw new UnsupportedChain(chain);
+    default:
+      throw new UnsupportedChain(chain);
+  }
+  return address;
 };
