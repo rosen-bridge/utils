8fd068e9441cd0127db5079904863489fc6e7636
HFazelinia <hadifazelinia78@gmail.com>
2024-02-10 16:40:38 +0000

    improve bitcoin rosen-extractor

--- packages/rosen-extractor/lib/getRosenData/bitcoin/BitcoinEsploraRosenExtractor.ts
+++ packages/rosen-extractor/lib/getRosenData/bitcoin/BitcoinEsploraRosenExtractor.ts
@@ -1,10 +1,6 @@
 import { RosenData, TokenTransformation } from '../abstract/types';
 import AbstractRosenDataExtractor from '../abstract/AbstractRosenDataExtractor';
-import {
-  BITCOIN_CHAIN,
-  BITCOIN_NATIVE_TOKEN,
-  SUPPORTED_CHAINS,
-} from '../const';
+import { BITCOIN_CHAIN, BITCOIN_NATIVE_TOKEN } from '../const';
 import {
   BitcoinEsploraTransaction,
   EsploraTxOutput,
@@ -12,8 +8,8 @@ import {
 } from './types';
 import { RosenTokens } from '@rosen-bridge/tokens';
 import { AbstractLogger } from '@rosen-bridge/abstract-logger';
-import { decodeAddress } from '@rosen-bridge/address-codec';
 import { address } from 'bitcoinjs-lib';
+import { parseRosenData } from './utils';
 
 export class BitcoinEsploraRosenExtractor extends AbstractRosenDataExtractor<BitcoinEsploraTransaction> {
   protected lockScriptPubKey: string;
@@ -51,7 +47,7 @@ export class BitcoinEsploraRosenExtractor extends AbstractRosenDataExtractor<Bit
       // parse rosen data from OP_RETURN box
       let opReturnData: OpReturnData;
       try {
-        opReturnData = this.parseRosenData(outputs[0].scriptpubkey);
+        opReturnData = parseRosenData(outputs[0].scriptpubkey);
       } catch (e) {
         this.logger.debug(
           baseError + `: Failed to extract data from OP_RETURN box: ${e}`
@@ -92,59 +88,6 @@ export class BitcoinEsploraRosenExtractor extends AbstractRosenDataExtractor<Bit
       }
     }
     return undefined;
-  };
-
-  /**
-   * extracts rosen data from OP_RETURN box script pub key
-   * @param scriptPubKeyHex
-   */
-  parseRosenData = (scriptPubKeyHex: string): OpReturnData => {
-    // check OP_RETURN opcode
-    if (scriptPubKeyHex.slice(0, 2) !== '6a')
-      throw Error(`script does not start with OP_RETURN opcode (6a)`);
-    // check OP_PUSHDATA1 opcode
-    if (scriptPubKeyHex.slice(2, 4) !== '4c')
-      throw Error(`script 2nd opcode is not OP_PUSHDATA1 (4c)`);
-    // check script length (should not use more than one OP_RETURN)
-    const dataLength = scriptPubKeyHex.slice(4, 6);
-    if (parseInt(dataLength, 16) + 3 !== scriptPubKeyHex.length / 2)
-      throw Error(
-        `script length is unexpected [${parseInt(dataLength, 16) + 3} !== ${
-          scriptPubKeyHex.length / 2
-        }]`
-      );
-
-    // parse toChain
-    const toChainHex = scriptPubKeyHex.slice(6, 8);
-    const toChainCode = parseInt(toChainHex, 16);
-    if (toChainCode >= SUPPORTED_CHAINS.length)
-      throw Error(
-        `invalid toChain code, found [${toChainCode}] but only [${SUPPORTED_CHAINS.length}] chains are supported`
-      );
-    const toChain = SUPPORTED_CHAINS[toChainCode];
-
-    // parse bridgeFee
-    const bridgeFeeHex = scriptPubKeyHex.slice(8, 24);
-    const bridgeFee = BigInt('0x' + bridgeFeeHex).toString();
-
-    // parse networkFee
-    const networkFeeHex = scriptPubKeyHex.slice(24, 40);
-    const networkFee = BigInt('0x' + networkFeeHex).toString();
-
-    // parse toAddress
-    const addressLengthCode = scriptPubKeyHex.slice(40, 42);
-    const addressHex = scriptPubKeyHex.slice(
-      42,
-      42 + parseInt(addressLengthCode, 16) * 2
-    );
-    const toAddress = decodeAddress(toChain, addressHex);
-
-    return {
-      toChain,
-      toAddress,
-      bridgeFee,
-      networkFee,
-    };
   };
 
   /**
