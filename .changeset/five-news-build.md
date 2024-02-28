812bcf763a8c514c668c949a098dfe38b837d577
fateme.r <fatemeh.r3366@gmail.com>
2024-02-28 10:54:39 +0330

    Update wid health check to be compatible with latest contract changes

---
+++ .changeset/five-news-build.md
@@ -0,0 +1,5 @@
+---
+'@rosen-bridge/health-check': major
+---
+
+Update wid health check to be compatible with latest contract changes
--- packages/health-check/tests/params/widHealthCheck/ExplorerWidHealthCheck.spec.ts
+++ packages/health-check/tests/params/widHealthCheck/ExplorerWidHealthCheck.spec.ts
@@ -1,14 +1,18 @@
 import ergoExplorerClientFactory from '@rosen-clients/ergo-explorer';
+import * as wasm from 'ergo-lib-wasm-nodejs';
+
 import { TestExplorerWidHealthCheck } from './TestExplorerWidHealthCheck';
 
 jest.mock('@rosen-clients/ergo-explorer');
 
 describe('ExplorerWidHealthCheckParam', () => {
   describe('update', () => {
+    const wid =
+      'e39047fa7025f5eb94f0b1a9d6d4728b5a4270ea1155e4e5b0e265db46589d5c';
     /**
      * @returns a mocked balance api function including a wid
      */
-    const mockedBalanceWithWid = () => {
+    const mockedBalanceWithWid = (wid: string) => {
       const mockedBalanaceFunc = jest.fn();
       mockedBalanaceFunc.mockReturnValue({
         tokens: [
@@ -18,8 +22,7 @@ describe('ExplorerWidHealthCheckParam', () => {
             amount: 81072,
           },
           {
-            tokenId:
-              'e39047fa7025f5eb94f0b1a9d6d4728b5a4270ea1155e4e5b0e265db46589d5c',
+            tokenId: wid,
             amount: 1,
           },
         ],
@@ -31,39 +34,21 @@ describe('ExplorerWidHealthCheckParam', () => {
      * @param repoNft
      * @returns a mocked repo box with correct nft and registrt value
      */
-    const mockedRepoBox = (repoNft: string) => {
+    const mockedCollateralBox = (awcNft: string, wid: string) => {
       return {
         assets: [
           {
-            tokenId: repoNft,
+            tokenId: awcNft,
             amount: 1n,
           },
         ],
         additionalRegisters: {
           R4: {
-            serializedValue:
-              '1a0b046572676f20c1f5aa4b1a713396a63f556df49c70add93b3471cbca893c6146fce5a4b95e7620d4e03eda58a338f8f65b40de258407dbdbbd9b8ccca374f66be8d97e52c8a75220ed6b8b63d187198f3ba55468231b5c83c050f7273364211fcf3b1ca7526ff30220846f72637b75f4817eb3f6e8542cb2d92875cf1ddce0fd5b734fc7c9bb48ac6020101f5f0995d90c80a9491815571ed1c9fc5522922fa3bbccbd575d1aa7255f9020e09921d0a87bb63e94aa6074c090a903a280ebd8cfba862396e56668f7138fe120730488119f223822c77c9c89fa73e1794116dd1503fcabf3fc9217ffe7b7b50820da06f26f55cca0ad06880e0e8fe40d4011373e526f6aafe47caba8f5fa16970720d206d6f37bc63fae26fbb591d5d4b60181db35fb712ae6dfa2c5faf0078d66eb20e39047fa7025f5eb94f0b1a9d6d4728b5a4270ea1155e4e5b0e265db46589d5c',
-          },
-        },
-      };
-    };
-
-    /**
-     * @returns a fake repobox with incorrect token id
-     */
-    const mockedFakeRepoBox = () => {
-      return {
-        assets: [
-          {
-            tokenId:
-              '3ac8a90d0aa8c5c50e99dd2588a990fd37b5d3aee70e32d56241f41ed49e9f03',
-            amount: 1n,
-          },
-        ],
-        additionalRegisters: {
-          R4: {
-            serializedValue:
-              '1a0b046572676f20c1f5aa4b1a713396a63f556df49c70add93b3471cbca893c6146fce5a4b95e7620d4e03eda58a338f8f65b40de258407dbdbbd9b8ccca374f66be8d97e52c8a75220ed6b8b63d187198f3ba55468231b5c83c050f7273364211fcf3b1ca7526ff30220846f72637b75f4817eb3f6e8542cb2d92875cf1ddce0fd5b734fc7c9bb48ac6020101f5f0995d90c80a9491815571ed1c9fc5522922fa3bbccbd575d1aa7255f9020e09921d0a87bb63e94aa6074c090a903a280ebd8cfba862396e56668f7138fe120730488119f223822c77c9c89fa73e1794116dd1503fcabf3fc9217ffe7b7b50820da06f26f55cca0ad06880e0e8fe40d4011373e526f6aafe47caba8f5fa16970720d206d6f37bc63fae26fbb591d5d4b60181db35fb712ae6dfa2c5faf0078d66eb20e39047fa7025f5eb94f0b1a9d6d4728b5a4270ea1155e4e5b0e265db46589d5c',
+            serializedValue: Buffer.from(
+              wasm.Constant.from_byte_array(
+                Buffer.from(wid, 'hex')
+              ).sigma_serialize_bytes()
+            ).toString('hex'),
           },
         },
       };
@@ -82,14 +67,21 @@ describe('ExplorerWidHealthCheckParam', () => {
      * - The widExists should update to true successfully using explorer api
      */
     it('Should update the wid status to exists', async () => {
+      console.log(mockedCollateralBox('awcNft', wid));
       jest.mocked(ergoExplorerClientFactory).mockReturnValue({
         v1: {
           // mock return value of explorer watcher address assets
-          getApiV1AddressesP1BalanceConfirmed: mockedBalanceWithWid(),
+          getApiV1AddressesP1BalanceConfirmed: mockedBalanceWithWid(wid),
           // mock return value of explorer address
           getApiV1BoxesUnspentByaddressP1: async () => {
             return {
-              items: [mockedRepoBox('rwtRepoNft'), mockedFakeRepoBox()],
+              items: [
+                mockedCollateralBox('awcNft', wid),
+                mockedCollateralBox(
+                  'awcNft',
+                  '74f3775de5fe5002c197311ebb80eaa4346646f24d8381b1bf271c11fd2ee095'
+                ),
+              ],
               total: 2,
             };
           },
@@ -98,8 +90,8 @@ describe('ExplorerWidHealthCheckParam', () => {
 
       // create new instance of ErgoExplorerWidHealthCheck
       const widHealthCheckParam = new TestExplorerWidHealthCheck(
-        'rwtRepoAddress',
-        'rwtRepoNft',
+        'collateralAddress',
+        'awcNft',
         'address',
         'explorer_url'
       );
@@ -125,21 +117,11 @@ describe('ExplorerWidHealthCheckParam', () => {
       jest.mocked(ergoExplorerClientFactory).mockReturnValue({
         v1: {
           // mock return value of explorer watcher address assets
-          getApiV1AddressesP1BalanceConfirmed: async () => {
-            return {
-              tokens: [
-                {
-                  tokenId:
-                    '10278c102bf890fdab8ef5111e94053c90b3541bc25b0de2ee8aa6305ccec3de',
-                  amount: 81072,
-                },
-              ],
-            };
-          },
+          getApiV1AddressesP1BalanceConfirmed: mockedBalanceWithWid('no-wid'),
           // mock return value of explorer address
           getApiV1BoxesUnspentByaddressP1: async () => {
             return {
-              items: [mockedRepoBox('rwtRepoNft')],
+              items: [mockedCollateralBox('awcNft', wid)],
               total: 1,
             };
           },
@@ -148,8 +130,8 @@ describe('ExplorerWidHealthCheckParam', () => {
 
       // create new instance of ErgoExplorerWidHealthCheck
       const widHealthCheckParam = new TestExplorerWidHealthCheck(
-        'rwtRepoAddress',
-        'rwtRepoNft',
+        'collateralAddress',
+        'awcNft',
         'address',
         'explorer_url'
       );
@@ -176,7 +158,7 @@ describe('ExplorerWidHealthCheckParam', () => {
       jest.mocked(ergoExplorerClientFactory).mockReturnValue({
         v1: {
           // mock return value of explorer watcher address assets
-          getApiV1AddressesP1BalanceConfirmed: mockedBalanceWithWid(),
+          getApiV1AddressesP1BalanceConfirmed: mockedBalanceWithWid(wid),
           getApiV1BoxesUnspentByaddressP1: boxesApiMock,
         },
       } as any);
@@ -184,17 +166,17 @@ describe('ExplorerWidHealthCheckParam', () => {
       // mock return value of explorer address
       boxesApiMock
         .mockReturnValueOnce({
-          items: [mockedFakeRepoBox()],
+          items: [mockedCollateralBox('fakeAwc', wid)],
           total: 103,
         })
         .mockReturnValueOnce({
-          items: [mockedRepoBox('rwtRepoNft')],
+          items: [mockedCollateralBox('awcNft', wid)],
           total: 103,
         });
 
       const widHealthCheckParam = new TestExplorerWidHealthCheck(
-        'rwtRepoAddress',
-        'rwtRepoNft',
+        'collateralAddress',
+        'awcNft',
         'address',
         'explorer_url'
       );
