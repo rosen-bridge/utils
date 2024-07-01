8cc344a10d0d24421cc2c8d0d807cefe9c948111
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2024-07-01 15:30:07 +0330

    remove tss package

--- packages/tss/lib/const/detection.ts
+++
@@ -1,7 +0,0 @@
-import { DetectionMessageType } from '../types/detection';
-
-const approveMessage: DetectionMessageType = 'approval';
-const heartbeatMessage: DetectionMessageType = 'heartbeat';
-const registerMessage: DetectionMessageType = 'register';
-
-export { approveMessage, heartbeatMessage, registerMessage };
--- packages/tss/lib/const/signer.ts
+++
@@ -1,9 +0,0 @@
-import { SignMessageType } from '../types/signer';
-
-const approveMessage: SignMessageType = 'approve';
-const requestMessage: SignMessageType = 'request';
-const startMessage: SignMessageType = 'start';
-const signUrl = 'sign';
-const thresholdUrl = 'threshold';
-
-export { approveMessage, requestMessage, startMessage, signUrl, thresholdUrl };
