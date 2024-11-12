5d36f60bec3226aee91f8eed071789621b32759c
mehran <mehran@protonmail.com>
2024-11-11 16:36:24 -0800

    doge-utxo-selection

---
+++ packages/doge-utxo-selection/lib/constants.ts
@@ -0,0 +1,5 @@
+// Constants for Dogecoin transaction size calculations
+export const DOGE_TX_BASE_SIZE = 10;
+export const DOGE_INPUT_SIZE = 148;
+export const DOGE_OUTPUT_SIZE = 34;
+export const MINIMUM_UTXO_VALUE = BigInt(100000); // 0.001 DOGE in satoshis
