921f57bca5ec4bfd4e6fca9b53d145676fa9d7df
HFazelinia <hadifazelinia78@gmail.com>
2024-12-04 12:27:07 +0000

    add Binance to address-codec

--- packages/address-codec/lib/const.ts
+++ packages/address-codec/lib/const.ts
@@ -2,3 +2,4 @@ export const BITCOIN_CHAIN = 'bitcoin';
 export const CARDANO_CHAIN = 'cardano';
 export const ERGO_CHAIN = 'ergo';
 export const ETHEREUM_CHAIN = 'ethereum';
+export const BINANCE_CHAIN = 'binance';
