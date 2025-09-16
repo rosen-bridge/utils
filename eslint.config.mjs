68ed3d2a5fdbde3cf1c0b1bb2107c1e6497bebf3
HFazelinia <hadifazelinia78@gmail.com>
2025-09-16 14:04:31 +0100

    fix eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import pluginJs from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import pluginCheckFile from 'eslint-plugin-check-file';
+import vitestPlugin from '@vitest/eslint-plugin';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    // General Ignore Patterns
+    ignores: ['**/dist/*', '**/node_modules/*'],
+  },
+  pluginJs.configs.recommended,
+  {
+    files: ['{services,packages}/**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      // Base Configuration for JS/TS Files
+      parser: typescriptParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        // Node-Specific Globals
+        ...globals.node,
+        ...vitestPlugin.environments.env.globals,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      'check-file': pluginCheckFile,
+      vitest: vitestPlugin,
+    },
+    rules: {
+      'check-file/filename-naming-convention': [
+        'error',
+        { '**/*.{js,ts,jsx,tsx}': 'CAMEL_CASE' },
+        { ignoreMiddleExtensions: true },
+      ],
+      // vitest Rules
+      ...vitestPlugin.configs.recommended.rules,
+    },
+  },
+  // Integrate Prettier for Formatting
+  prettier,
+];
