bfb5a4b7ff273b6d4a58212ddea186ed3d486083
Mohsen Javidpanah <moshen212@gmail.com>
2025-10-02 09:20:46 +0330

    Fixed lint problem

--- packages/rosen-extractor/lib/getRosenData/ergo/ergoNodeRosenExtractor.ts
+++ packages/rosen-extractor/lib/getRosenData/ergo/ergoNodeRosenExtractor.ts
@@ -49,7 +49,7 @@ export class ErgoNodeRosenExtractor extends AbstractRosenDataExtractor<NodeTrans
                   amount: assetTransformation.amount,
                   targetChainTokenId: assetTransformation.to,
                   sourceTxId: transaction.id,
-                  rawData: R4Serialized.map(_ => _.toString()).join(','),
+                  rawData: R4Serialized.map((_) => _.toString()).join(','),
                 };
               }
             } else
