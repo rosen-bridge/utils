c69ac296de3cf30801552f8aeec7bfc2b579275a
Mohsen Javidpanah <moshen212@gmail.com>
2025-10-23 11:06:10 +0330

    Updated codes after MR reviewed

--- packages/boxes/minimum-fee/tests/setup.ts
+++ packages/boxes/minimum-fee/tests/setup.ts
@@ -1,6 +1,5 @@
 vi.mock('@rosen-clients/ergo-explorer', () => {
   return {
-    __esModule: true,
     default: vi.fn(() => ({
       v1: {
         getApiV1BoxesUnspentBytokenidP1: vi.fn(),
@@ -11,7 +10,6 @@ vi.mock('@rosen-clients/ergo-explorer', () => {
 
 vi.mock('@rosen-clients/ergo-node', () => {
   return {
-    __esModule: true,
     default: vi.fn(() => ({
       getBoxesByTokenIdUnspent: vi.fn(),
     })),
