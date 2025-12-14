58c9698d386af7b7c0ce80d8dfa7ceefdca2e516
HFazelinia <hadifazelinia78@gmail.com>
2025-12-14 10:20:42 +0000

    remove redundant directory in tokens package

--- packages/tokens/lib/index.ts
+++ packages/tokens/lib/index.ts
@@ -1,3 +1,3 @@
-export { TokenMap } from './TokenMap/tokenMap';
-export * from './TokenMap/types';
-export * from './TokenMap/constants';
+export { TokenMap } from './tokenMap';
+export * from './types';
+export * from './constants';
