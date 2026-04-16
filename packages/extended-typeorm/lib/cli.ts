b9ae9715e4bded7ef1e4edbee8408e5031028392
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2026-04-16 15:24:09 +0330

    feat: add migrate command in cli.ts

--- packages/extended-typeorm/lib/cli.ts
+++ packages/extended-typeorm/lib/cli.ts
@@ -7,82 +7,12 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
 yargs(hideBin(process.argv))
-  .command(
-    'generate:sqlite',
-    'generates a SQLite migration for the project',
-    (yargs) =>
-      yargs.option('output', {
-        alias: 'o',
-        demandOption: true,
-        description: 'output path for the SQLite migration',
-        type: 'string',
-      }),
-    async (argv) => {
-      const spinner = ora();
-      spinner.start('Generating SQLite migration');
-
-      try {
-        execSync(
-          `npx run typeorm migration:generate ${argv.output} -- -p -d ./config/dataSource.ts`,
-          { stdio: 'inherit' },
-        );
-        spinner.succeed(
-          chalk.green(`SQLite migration generated at "${argv.output}"`),
-        );
-      } catch (error) {
-        spinner.fail(chalk.red('Failed to generate SQLite migration'));
-        console.error(error);
-      }
-    },
-  )
-  .command(
-    'generate:postgres',
-    'generates a PostgreSQL migration for the project',
-    (yargs) =>
-      yargs.option('output', {
-        alias: 'o',
-        demandOption: true,
-        description: 'output path for the PostgreSQL migration',
-        type: 'string',
-      }),
-    async (argv) => {
-      const spinner = ora();
-      spinner.start('Generating PostgreSQL migration');
-
-      try {
-        execSync(
-          `npm run typeorm migration:generate ${argv.output} -- -p -d ./config/dataSource.ts`,
-          { stdio: 'inherit' },
-        );
-        spinner.succeed(
-          chalk.green(`PostgreSQL migration generated at "${argv.output}"`),
-        );
-      } catch (error) {
-        spinner.fail(chalk.red('Failed to generate PostgreSQL migration'));
-        console.error(error);
-      }
-    },
-  )
-  .command('migrate', 'runs the migration for the project', async () => {
-    const spinner = ora();
-    spinner.start('Running migrations');
-
-    try {
-      execSync('npm run typeorm migration:run -- -d ./config/dataSource.ts', {
-        stdio: 'inherit',
-      });
-      spinner.succeed(chalk.green('Migrations run successfully'));
-    } catch (error) {
-      spinner.fail(chalk.red('Failed to run migrations'));
-      console.error(error);
-    }
-  })
-  .command('run', 'runs TypeORM CLI', async () => {
+  .command('$0', 'runs TypeORM CLI', {}, () => {
     const spinner = ora();
     spinner.start('Running TypeORM CLI');
-
+    let argv = process.argv.slice(2).join(' ');
     try {
-      execSync('npx typeorm', {
+      execSync(`typeorm ${argv}`, {
         stdio: 'inherit',
       });
       spinner.succeed(chalk.green('TypeORM CLI executed successfully'));
