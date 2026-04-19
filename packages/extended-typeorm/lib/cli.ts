0d9987bf9ef0914d2bc288c11e87be8a263f51eb
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2026-04-19 15:03:09 +0330

    fix: remove yargs package

--- packages/extended-typeorm/lib/cli.ts
+++ packages/extended-typeorm/lib/cli.ts
@@ -3,23 +3,18 @@
 import chalk from 'chalk';
 import { execSync } from 'child_process';
 import ora from 'ora';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
 
-yargs(hideBin(process.argv))
-  .command('$0', 'runs TypeORM CLI', {}, () => {
-    const spinner = ora();
-    spinner.start('Running TypeORM CLI');
-    let argv = process.argv.slice(2).join(' ');
-    try {
-      execSync(`typeorm ${argv}`, {
-        stdio: 'inherit',
-      });
-      spinner.succeed(chalk.green('TypeORM CLI executed successfully'));
-    } catch (error) {
-      spinner.fail(chalk.red('Failed to run TypeORM CLI'));
-      console.error(error);
-    }
-  })
-  .demandCommand(1)
-  .parse();
+const spinner = ora();
+spinner.start('Running TypeORM CLI');
+
+let argv = process.argv.slice(2).join(' ');
+
+try {
+  execSync(`typeorm ${argv}`, {
+    stdio: 'inherit',
+  });
+  spinner.succeed(chalk.green('TypeORM CLI executed successfully'));
+} catch (error) {
+  spinner.fail(chalk.red('Failed to run TypeORM CLI'));
+  console.error(error);
+}
