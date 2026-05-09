28b72707a52134957d1caf3f74b683c4bd50b216
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2026-05-09 13:01:39 +0330

    apply suggested changes from PR review

--- packages/config/lib/config.ts
+++ packages/config/lib/config.ts
@@ -217,7 +217,6 @@ export class ConfigValidator {
           for (let i = 0; i < value.length; i++) {
             const item = value[i];
             const itemPath = childPath.concat([String(i)]);
-
             if (activeField.items.type === 'object') {
               this.validateSubConfig(
                 config,
